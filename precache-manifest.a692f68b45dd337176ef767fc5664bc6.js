28b2e078e8c9bd779d27df866f533023ec16dbeb
Stuartkal <Stuartkal@gmail.com>
2019-07-25 00:03:44 +0300

    Updates

---
+++ precache-manifest.a692f68b45dd337176ef767fc5664bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d668bdf6d12f7e15d7833a43562cca9b",
+    "url": "/lyricsearch/index.html"
+  },
+  {
+    "revision": "a8b47040a2a6f1a60de1",
+    "url": "/lyricsearch/static/css/main.5a50780e.chunk.css"
+  },
+  {
+    "revision": "e92c0b440515c0343799",
+    "url": "/lyricsearch/static/js/2.3272996f.chunk.js"
+  },
+  {
+    "revision": "a8b47040a2a6f1a60de1",
+    "url": "/lyricsearch/static/js/main.8966fee8.chunk.js"
+  },
+  {
+    "revision": "d96b37bab5ead26ad5c6",
+    "url": "/lyricsearch/static/js/runtime~main.e411086c.js"
+  },
+  {
+    "revision": "fc54e0c4602d96f301262e5500d789f3",
+    "url": "/lyricsearch/static/media/Spinner.fc54e0c4.gif"
+  }
+]);
